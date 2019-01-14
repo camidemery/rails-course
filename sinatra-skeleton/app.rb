06d15cdab00c074170bae8b7e8d917ca952237bf
Cami Demery <cami.demery@gmail.com>
2019-01-14 18:23:22 +0000

    testing

--- sinatra-skeleton/app.rb
+++ sinatra-skeleton/app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-set :database, {adapter: 'sqlite3', database: 'test.db'}
+set :database, {adapter:'sqlite3', database:'users.db'}
 
 #then set up sinatra actions
 
