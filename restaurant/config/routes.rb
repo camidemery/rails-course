7ed270b026a9874fb44b3c7484adf719ea6fe3c8
Cami Demery <cami.demery@gmail.com>
2019-02-12 00:03:37 +0000

    restaurant part 1

--- restaurant/config/routes.rb
+++ restaurant/config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :dishes
+  
+  resources :orders do
+    member do #members means we are looking for an id
+      get 'prepare'
+      #'/orders/:id/prepare'
+      post 'add'
+      #'/orders/:id/add'
+      delete 'remove'
+    end
+    #collection do would render an id-less URL
+    
+  end
+  
+  root 'orders#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
